8260d21b68464d8d2428cca822846f94717db5b2
SandrLite <mr.wlas@mail.ru>
2023-10-13 08:20:11 +0300

    Добавьте файлы проекта.

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DiveIntoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                int result = 0;
+                int number1 = int.Parse(args[0]);
+                string operation = args[1];
+                int number2 = int.Parse(args[2]);
+                if (operation == "+")
+                {
+                    result = number1 + number2;
+                }
+                if (operation == "-")
+                {
+                    result = number1 - number2;
+                }
+                if (operation == "/")
+                {
+                    result = number1 / number2;
+                }
+                if (operation == "*")
+                {
+                    result = number1 * number2;
+                }
+                Console.WriteLine("Результат выполнения операции (" + operation + ") = " + result);
+            }
+            else
+                Console.WriteLine("Для использования программы введите: число знак(+-/*) число");
+        }
+    }
+}
