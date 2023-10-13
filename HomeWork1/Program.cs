e15d41c712d4fb5b7781474b8bb43c740101a7ea
SandrLite <mr.wlas@mail.ru>
2023-10-13 08:29:55 +0300

    HomeWork2

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -4,32 +4,31 @@
     {
         static void Main(string[] args)
         {
-            if (args.Length == 3)
+            int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+            int y = a.GetLength(0);
+            int x = a.GetLength(1);
+            int[] sort = new int[x * y];
+            int counter = 0;
+            for (int i = 0; i < y; i++)
             {
-                int result = 0;
-                int number1 = int.Parse(args[0]);
-                string operation = args[1];
-                int number2 = int.Parse(args[2]);
-                if (operation == "+")
+                for (int j = 0; j < x; j++)
                 {
-                    result = number1 + number2;
+                    sort[counter] = a[i, j];
+                    counter++;
                 }
-                if (operation == "-")
-                {
-                    result = number1 - number2;
-                }
-                if (operation == "/")
-                {
-                    result = number1 / number2;
-                }
-                if (operation == "*")
-                {
-                    result = number1 * number2;
-                }
-                Console.WriteLine("Результат выполнения операции (" + operation + ") = " + result);
             }
-            else
-                Console.WriteLine("Для использования программы введите: число знак(+-/*) число");
+            Array.Sort(sort);
+            counter = 0;
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    a[i, j] = sort[counter];
+                    counter++;
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
